4bf44f0f980df4011c3dd9aa381005ef38305568
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-03-06 11:34:28 +0700

    style contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,12 +4,16 @@ import React from 'react';
 import {useFormik} from 'formik';
 import * as Yup from "yup";
 import ContactBox from "../components/Fragments/ContactBox";
+import emailjs from '@emailjs/browser';
+
 
 const ContactPage = () => {
    const contactForm =() => {
-      // e.preventDefault();
       alert("Form Submit ")
-      // alert(formik.values.textarea);
+
+      // emailjs.sendForm("service_yu53vcb","template_ltyjpfe", form.current, {
+      //    publicKey:"OLBtCJ2If6lUW2SUx",
+      //  })
    }
    const formik = useFormik({
       initialValues:{
@@ -19,10 +23,10 @@ const ContactPage = () => {
       },
       onSubmit : contactForm,
       validationSchema: Yup.object().shape({
-         fullname: Yup.string().required().max(15 , "Max caracter 15").min(3, "min caracter 3"),
+         fullname: Yup.string().required().max(15 , "Max char 15").min(3, "min caracter 3"),
          email: Yup.string().required().email(),
-         textarea : Yup.string().required().max(30, "maxsimal 30 char")
-      })
+         textarea : Yup.string().required().min(10, "Min 10 char")
+      }),
    });
 
    const handleForm = (event) => {
@@ -42,7 +46,9 @@ const ContactPage = () => {
 
                <form onSubmit={formik.handleSubmit}>
                   <InputForm.Label htmlFor="fullname">Fullname</InputForm.Label>
-                  <input id="fullname" type="text" name="fullname"
+                  <input 
+                  className="px-3 py-2 border border-slate-500 rounded outline-blue-400 "
+                  type="text" name="fullname" id="fullname"
                   onChange={handleForm}
                   />
                   {formik.touched.fullname && formik.errors.fullname ? (
@@ -50,7 +56,9 @@ const ContactPage = () => {
                   ): null }
 
                   <InputForm.Label htmlFor="email">Email</InputForm.Label>
-                  <input type="email" name="email"
+                  <input
+                  className="px-3 py-2 border border-slate-500 rounded outline-blue-400 "
+                  type="email" name="email" id="email"
                   onChange={handleForm}
                   />
                   {formik.touched.email && formik.errors.email ? (
@@ -58,14 +66,22 @@ const ContactPage = () => {
                   ): null }
 
                   <InputForm.Label htmlFor="textarea">Textarea Suggestion</InputForm.Label>
-                  <textarea name="textarea"
+                  <textarea
+                  className="px-3 py-2 border border-slate-500 rounded outline-blue-400"
+                  name="textarea" id="textarea"
                   onChange={handleForm}
                   />
                   {formik.touched.textarea && formik.errors.textarea ? (
                      <div>{formik.errors.textarea}</div>
                   ): null }
+
+                  <div>
+                     <button
+                        className="bg-blue-500"
+                        type="submit">Submit
+                     </button>
+                  </div>
                   
-                  <button type="submit">Submit</button>
                </form>
 
             </div>
